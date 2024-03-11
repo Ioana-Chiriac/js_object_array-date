2dda0abd4ae2c48f6ca207517e011e4fd37763dd
Ioana-Chiriac <ioana.chiriac1014@gmail.com>
2024-03-11 18:57:14 +0200

    final array (conditioanle si bucle)

--- script.js
+++ script.js
@@ -11,3 +11,107 @@ console.log( `my product after push`, myProducts);
 myProducts.shift();
 console.log(`myproduct after shift():`, myProducts);
 
+// tema curs 9 din urma
+
+let student = true;
+console.log(student);
+
+let sirA = [`a`, `b`, `c`];
+let sirB = [`1`, `2`, `3`];
+let sirConcatenat = sirA +' '+ sirB;
+console.log(sirConcatenat);
+
+let nr1 = 5;
+let nr2 = 3;
+let rest = nr1 % nr2;
+console.log(rest);
+
+let name;
+console.log(name);
+
+let lungime = 2
+let latime = 5;
+console.log(latime * lungime);
+
+const sir1 = ['clasa', 'ora', 'mate'];
+const sir2 = ['clasa', 'ora', 'mate'];
+const sirif = sir1 == sir2; 
+console.log(sirif);
+
+const sir3 = 1234;
+const sir4 = 1234;
+const sirif2 = sir3 == sir4; 
+console.log(sirif2);
+
+let birthyear = 1999
+let currentyear = new Date().getFullYear();
+let age = currentyear - birthyear;
+console.log(`my age is ${age}`);
+
+let numeC = 'claudia';
+let numeD = 'cristina';
+let myfullname = numeC +' '+ numeD;
+let uppercaseName = myfullname.toUpperCase();
+console.log(uppercaseName);
+
+let number = 134;
+let comparison = number >= 100;
+console.log(comparison);
+// object
+
+let person = {
+    name: "John",
+    surname: "Doe",
+    address: {
+        street: "Cetatii",
+        appartament: "b1",
+        block: "7b",
+        tronson: 1,
+        zipcode: "100203",
+        city: "Cluj-Napoca",
+        Country: "Romania",
+        County: "Cluj"
+    },
+    age: 32
+};
+console.log(`persone:`, person);
+console.log(`nume persoane: ${person.name}`);
+console.log(`cod postal: ${person.address.zipcode}`);
+
+// bracket notation
+console.log(`persone surename: ${person["surname"]}`);
+
+// modificare valori 
+person.name = "Ion";
+console.log(`persone name:`, person.name);
+
+
+// comparare obiecte 
+const obj1 = {a: 10, b: true};
+const obj2 = {a: 10, b: true};
+const obj3 = obj1;
+
+console.log(`obj1 === obj2`, obj1 === obj2);
+console.log(`obj1 === obj3`, obj1 === obj3);
+
+// comparar dupa propietati
+const areEqual = obj1.a === obj2.a && obj2.b === obj3.b;
+console.log(`areEqual:`, areEqual);
+
+//stergere 
+
+delete person.age;
+console.log(person);
+
+// afisare data
+const date = new Date();
+console.log(`date:`, date);
+
+// acesare an
+console.log(`an`, date.getFullYear());
+
+// acesare luna
+const months = ["Ianuarie", "Februaie", "Martie"];
+
+console.log
+
